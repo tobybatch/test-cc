ef289961f414c48a2c9638bb8e1155a9abdf92fd
Toby Batch <tobias@neontribe.co.uk>
2026-09-14 11:10:07 +0100

    adds a workflow for pr linting

--- bin/lint-pr-commits.js
+++ bin/lint-pr-commits.js
@@ -21,6 +21,7 @@ if (commits.length === 0) {
 
 for (const sha of commits) {
   const message = execSync(`git log --format=%s -n 1 ${sha}`, { encoding: 'utf8' }).trim();
+  console.log(message);
 
   try {
     execSync(`npx commitlint --config commitlint.config.js --edit`, {
