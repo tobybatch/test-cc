63787d66d134a7fe19866a3299ff62dadb381830
Toby Batch <tobias@neontribe.co.uk>
2026-09-14 10:11:44 +0100

    style: serz apnd zdqy jsdo undt jkpe

--- bin/generate-commits.js
+++ bin/generate-commits.js
@@ -14,41 +14,21 @@ function generateWord() {
     return word;
 }
 
-function runShellCommand(command) {
-    return new Promise((resolve, reject) => {
-        childProcess.exec(command, (error, stdout, stderr) => {
-            if(error) {
-                console.error(`exec error: ${error}`);
-                reject(error);
-            }
-            resolve({stdout, stderr});
-        });
-    });
-}
-
-for (let i = 1; i <= parseInt(count); i++) {
-    let message = "";
-    if (Math.random() < temperature) {
-        let randomTypeIndex = Math.floor(Math.random() * types.length);
-        message += `${types[randomTypeIndex]}`;
+setTimeout(() => {
+    for (let i = 1; i <= parseInt(count); i++) {
+        let message = "";
         if (Math.random() < temperature) {
-            message += `(${generateWord()})`;
+            let randomTypeIndex = Math.floor(Math.random() * types.length);
+            message += `${types[randomTypeIndex]}`;
+            if (Math.random() < temperature) {
+                message += `(${generateWord()})`;
+            }
+            message += ": ";
         }
-        message += ": ";
+        for (let j = 1; j <= 6; j++) {
+            message += generateWord() + " ";
+        }
+        childProcess.exec("git add .");
+        childProcess.exec(`git commit -am "${message}"`);
     }
-    for (let j = 1; j <= 6; j++) {
-        message += generateWord() + " ";
-    }
-    runShellCommand("git add .").then((result) => {
-        console.log(`stdout: ${result.stdout}`);
-        console.log(`stderr: ${result.stderr}`);
-
-        return runShellCommand(`git commit -am "${message}"`);
-    }).then((result) => {
-        console.log(`stdout: ${result.stdout}`);
-        console.log(`stderr: ${result.stderr}`);
-    }).catch((error) => {
-        console.error(error);
-    });
-}
-
+}, 1000);
